2670701290c9a6695831b42c761f1e2c1190b7b9
Thomas Boers <thomasboers@hotmail.com>
2017-04-04 15:45:48 +0200

    training controller

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,7 +1,55 @@
 class TrainingsController < ApplicationController
+  before_action :load_training, only: [:show, :edit, :update, :destroy]
+
   def index
+    @training = Training.all
   end
 
   def show
   end
+
+  def new
+    @training = Training.new
+  end
+
+  def create
+    @training = Training.new(training_params)
+    @training.profile = current_user.profile
+    raise params
+    if @training.save
+      redirect_to trainings_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @training = Artwork.new(training_params)
+    @training.profile = current_user.profile
+    if @training.save
+      redirect_to trainings_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @training.destroy
+    redirect_to :back
+  end
+
+  private
+   def training_params
+    params.require(:training).permit(:title, :date, :time, :duration, :price, :group_capacity, :level, :description, :outdoor, :longitude, :lattitude, :country, :city, :address, :zip_code)
+  end
+
+  def load_training
+    @training = Training.find(params[:id])
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
-  
-  devise_for :users
+
   devise_for :users, controllers: { registrations: "registrations"}
-  
+
   resources :profiles, only: [:edit, :update]
   resources :trainers, except: [:new, :create]
-  
+
   resources :sports, only: [:index, :show]
-  resources :trainings, only: [:index, :show]
-  
-  
+  resources :trainings, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
