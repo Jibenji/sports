3d94cc9e4bf0a980f9f9880fd8906b294d7b5963
Benjamin Toulemonde <benjamin.toulemonde@ieseg.fr>
2017-04-04 13:57:33 +0200

    first working process of profile edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'profiles/index'
+
+  get 'profiles/show'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
+  get 'profiles/destroy'
+
+  devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#home'
+
+  resources :profiles, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
