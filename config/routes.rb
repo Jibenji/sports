605e8936b28eb09612bf9aa213989eb703ea4714
Thomas Boers <thomasboers@hotmail.com>
2017-04-04 13:09:22 +0200

    created trainer, sports en trainings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'trainers/index'
+
+  get 'trainers/show'
+
+  get 'trainers/edit'
+
+  get 'trainers/update'
+
+  get 'trainers/delete'
+
+  get 'trainings/index'
+
+  get 'trainings/show'
+
+  get 'sports/index'
+
+  get 'sports/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
