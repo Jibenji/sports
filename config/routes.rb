8fd522a6235173e6694d32897d9a2544fd8de585
Max Kraanen <max@maxkraanen.com>
2017-04-04 16:27:38 +0200

    finished controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :requests
   resources :trainers
 
-  resources :sports, only: [:index, :show]
+  resources :sports
 
   resources :trainings, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
