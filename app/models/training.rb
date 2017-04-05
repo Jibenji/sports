c9a51120bbc8c6963c176b795a598b4be1daf851
Benjamin Toulemonde <benjamin.toulemonde@ieseg.fr>
2017-04-05 12:36:46 +0200

    showing the correct number of available spots

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,4 +2,13 @@ class Training < ApplicationRecord
   belongs_to :sport
   belongs_to :profile
   has_many :bookings
+
+
+  def available_spots
+    total = 0
+    self.bookings.each do |booking|
+      total += booking.participants
+    end
+    self.group_capacity - total
+  end
 end
