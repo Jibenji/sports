91b080f080dd8d71d0d0596277af49bd18463cd2
Benjamin Toulemonde <benjamin.toulemonde@ieseg.fr>
2017-04-06 11:56:33 +0200

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,56 @@
-# Profile.destroy_all
-# Sport.destroy_all
-# Training.destroy_all
-# Booking.destroy_all
-# Review.destroy_all
-
-#users
 user = User.last
 
 #profiles
 profile = Profile.create(first_name: "Your Session", user: user)
+attributes = {
+  certificates: "My certificates",
+  bio: "My Bio",
+  education: "My education",
+  description: "My description",
+  banner_picture: "test"
+}
+profile.trainer = Trainer.new(attributes)
+profile.save!
 #requester = Profile.create(first_name: "I want to be a trainer", user: user)
 
 #sports
-yoga = Sport.create(name: "Yoga", tagline: "Boring sheet", description: "You need to be flexible man...", banner_sport_picture: "image/upload/v1491328960/gfkd0vtljwxhc2dvkq0w.jpg")
+yoga = Sport.new(name: "Yoga", tagline: "Boring sheet", description: "You need to be flexible man...")
+url = "http://res.cloudinary.com/dvhelys1s/image/upload/v1491328960/gfkd0vtljwxhc2dvkq0w.jpg"
+yoga.remote_banner_sport_picture_url = url
 yoga.save!
-# tennis = Sport.create(name: "Tennis", tagline: "Bounce the ball", description: "We love Maria", banner_sport_picture: "image/upload/v1491328855/m0wnlxdholdjguy2h9vx.jpg")
-# boxing = Sport.create(name: "Boxing", tagline: "Have a punch", description: "You really need to like bruises", banner_sport_picture: "image/upload/v1491328720/m3i5ddcdlwopaqdsgjiq.jpg")
-# running = Sport.create(name: "Running", tagline: "Run forest run !", description: "A bit useless but enjoyeable", banner_sport_picture: "image/upload/v1491328912/y4qawhgl3k3pfcl8jsnp.jpg")
 
-# #trainings
-# yoga_training = Training.create(profile: profile, sport: yoga, title: "Yoga Training"         , price: 10, city: "Amsterdam")
-# tennis_training = Training.create(profile: profile, sport: tennis, title: "Tennis Training"   , price: 35, city: "Amsterdam")
-# boxing_training = Training.create(profile: profile, sport: boxing, title: "Boxing Training"   , price: 10, city: "Amsterdam")
-# running_training = Training.create(profile: profile, sport: running, title: "Running Training", price: 8, city: "Amsterdam")
+tennis = Sport.create(name: "Tennis", tagline: "Bounce the ball", description: "We love Maria")
+url = "http://res.cloudinary.com/dvhelys1s/image/upload/v1491328960/gfkd0vtljwxhc2dvkq0w.jpg"
+tennis.remote_banner_sport_picture_url = url
+tennis.save!
+
+boxing = Sport.create(name: "Boxing", tagline: "Have a punch", description: "You really need to like bruises")
+url = "http://res.cloudinary.com/dvhelys1s/image/upload/v1491328960/gfkd0vtljwxhc2dvkq0w.jpg"
+boxing.remote_banner_sport_picture_url = url
+boxing.save!
+
+running = Sport.create(name: "Running", tagline: "Run forest run !", description: "A bit useless but enjoyeable")
+url = "http://res.cloudinary.com/dvhelys1s/image/upload/v1491328960/gfkd0vtljwxhc2dvkq0w.jpg"
+running.remote_banner_sport_picture_url = url
+running.save!
+
+# #training
+yoga_training = Training.create(profile: profile, sport: yoga, title: "Yoga Training"         , price: 10, city: "Amsterdam", group_capacity: 5)
+tennis_training = Training.create(profile: profile, sport: tennis, title: "Tennis Training"   , price: 35, city: "Amsterdam", group_capacity: 6)
+boxing_training = Training.create(profile: profile, sport: boxing, title: "Boxing Training"   , price: 10, city: "Amsterdam", group_capacity: 8)
+running_training = Training.create(profile: profile, sport: running, title: "Running Training", price: 8,  city: "Amsterdam", group_capacity: 2)
 
 # #bookings
-# yoga_booking = Booking.create(training: yoga_training, participants: 7, profile: profile)
-# tennis_booking = Booking.create(training: tennis_training, participants: 2, profile: profile)
-# boxing_booking = Booking.create(training: boxing_training, participants: 3, profile: profile)
-# running_booking = Booking.create(training: running_training, participants: 5, profile: profile)
+yoga_booking = Booking.create(training: yoga_training, participants: 2, profile: profile)
+tennis_booking = Booking.create(training: tennis_training, participants: 2, profile: profile)
+boxing_booking = Booking.create(training: boxing_training, participants: 3, profile: profile)
+running_booking = Booking.create(training: running_training, participants: 1, profile: profile)
 
 # #reviews
-# yoga_review = Review.create(booking: yoga_booking, profile: profile)
-# tennis_review = Review.create(booking: tennis_booking, profile: profile)
-# boxing_review = Review.create(booking: boxing_booking, profile: profile)
-# running_review = Review.create(booking: running_booking, profile: profile)
+yoga_review = Review.create(booking: yoga_booking, profile: profile      , content: "this is great", rating: 4)
+tennis_review = Review.create(booking: tennis_booking, profile: profile  , content: "this is great", rating: 4)
+boxing_review = Review.create(booking: boxing_booking, profile: profile  , content: "this is great", rating: 4)
+running_review = Review.create(booking: running_booking, profile: profile, content: "this is great", rating: 4)
 
 # #requests
 # #request = Request.create(banner_picture: , bio: "I've been a requester all my life", description: "Requester's description")
