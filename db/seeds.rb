182c3c3764f46aa975e2be014818fcefa90e47bf
Thomas Boers <thomasboers@hotmail.com>
2017-04-12 14:07:08 +0200

    Finish homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,11 +40,11 @@ dancing.remote_banner_sport_picture_url = url
 dancing.save!
 
 # #training
-yoga_training = Training.create(latitude: 52.362886, longitude: 4.915362, profile: profile, sport: yoga, title: "Yoga Training"         , price: 10, city: "Amsterdam", group_capacity: 5, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 105, Amsterdam", duration: 60)
-bootcamp_training = Training.create(latitude: 52.359838, longitude: 4.863829, profile: profile, sport: bootcamp, title: "bootcamp Training"   , price: 35, city: "Amsterdam", group_capacity: 6, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 5, Amsterdam", duration: 60)
-boxing_training = Training.create(latitude: 52.348110, longitude: 4.904577, profile: profile, sport: boxing, title: "Boxing Training"   , price: 10, city: "Amsterdam", group_capacity: 8, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 205, Amsterdam", duration: 60)
-running_training = Training.create(latitude: 52.279128, longitude: 4.900272, profile: profile, sport: running, title: "Running Training", price: 8,  city: "Amsterdam", group_capacity: 2, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 80, Amsterdam", duration: 60)
-dancing_training = Training.create(latitude: 52.479128, longitude: 4.900272, profile: profile, sport: dancing, title: "DancingTraining", price: 8,  city: "Amsterdam", group_capacity: 2, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 80, Amsterdam", duration: 60)
+# yoga_training = Training.create(latitude: 52.362886, longitude: 4.915362, profile: profile, sport: yoga, title: "Yoga Training"         , price: 10, city: "Amsterdam", group_capacity: 5, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 105, Amsterdam", duration: 60)
+# bootcamp_training = Training.create(latitude: 52.359838, longitude: 4.863829, profile: profile, sport: bootcamp, title: "bootcamp Training"   , price: 35, city: "Amsterdam", group_capacity: 6, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 5, Amsterdam", duration: 60)
+# boxing_training = Training.create(latitude: 52.348110, longitude: 4.904577, profile: profile, sport: boxing, title: "Boxing Training"   , price: 10, city: "Amsterdam", group_capacity: 8, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 205, Amsterdam", duration: 60)
+# running_training = Training.create(latitude: 52.279128, longitude: 4.900272, profile: profile, sport: running, title: "Running Training", price: 8,  city: "Amsterdam", group_capacity: 2, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 80, Amsterdam", duration: 60)
+# dancing_training = Training.create(latitude: 52.479128, longitude: 4.900272, profile: profile, sport: dancing, title: "DancingTraining", price: 8,  city: "Amsterdam", group_capacity: 2, level: "difficult", outdoor: true, description: "Outdoor training in vondelpark with Jennifer", date: Date.today, time: Time.now, address: "Sarphatistraat 80, Amsterdam", duration: 60)
 
 # # #bookings
 # yoga_booking = Booking.create(training: yoga_training, participants: 2, profile: profile)
@@ -64,17 +64,17 @@ dancing_training = Training.create(latitude: 52.479128, longitude: 4.900272, pro
 # request = Request.create(banner_picture: , bio: "I've been a requester all my life", description: "Requester's description")
 
 
-puts "profile.to_json"
-puts profile.to_json
+# puts "profile.to_json"
+# puts profile.to_json
 
-puts "yoga.to_json"
-puts yoga.to_json
+# puts "yoga.to_json"
+# puts yoga.to_json
 
-puts "bootcamp_training.to_json"
-puts bootcamp_training.to_json
+# puts "bootcamp_training.to_json"
+# puts bootcamp_training.to_json
 
-puts "running_training.to_json"
-puts running_training.to_json
+# puts "running_training.to_json"
+# puts running_training.to_json
 
 # puts "boxing_review.to_json"
 # puts boxing_review.to_json
